a929e051b6aa620b9ec5044b3602363ab5f897fb
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-10 14:46:07 +0000

    Make button dynamic

--- resources/js/components/TrackingButton.js
+++ resources/js/components/TrackingButton.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class TrackingButton extends React.Component {
-    render () {
-        var buttonColour = (this.props.text === 'Stop') ? buttonColour = 'bg-red' : buttonColour = 'bg-green'
-        return (
-            <button className={buttonColour + '-200 rounded p-3 w-1/5 mt-5 shadow-md hover:shadow-lg hover:border-transparent hover:text-white hover:' + buttonColour +'-500'}>
-                {this.props.text} Tracking Boots and Bars Time
-            </button>
-        );
-    }
+function TrackingButton(props) {
+    const [tracking, setTracking] = useState(false);
+
+    var buttonColour = (tracking) ? buttonColour = 'bg-red' : buttonColour = 'bg-green'
+    return (
+        <button
+            className={buttonColour + '-200 rounded p-3 w-1/5 mt-5 shadow-md hover:shadow-lg hover:border-transparent hover:text-white hover:' + buttonColour +'-500'}
+            onClick={() => setTracking(!tracking)}>
+            {props.text} Tracking Boots and Bars Time
+        </button>
+    );
 }
 
 export default TrackingButton;
