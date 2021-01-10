69227fddb82aea1dc5ede634f71c911f84d06f94
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-10 14:53:10 +0000

    Make the transition nice

--- resources/js/components/NextCalendarEvent.js
+++ resources/js/components/NextCalendarEvent.js
@@ -3,7 +3,7 @@ import React from 'react';
 class NextCalenderEvent extends React.Component {
     render() {
         return (
-            <div className="mt-3 m-auto p-4 rounded bg-blue-200 inline-block shadow-md hover:shadow-lg hover:border-transparent hover:text-white hover:bg-blue-500 cursor-pointer">
+            <div className="mt-3 m-auto p-4 rounded bg-blue-200 inline-block shadow-md transition duration-500 hover:shadow-lg hover:border-transparent hover:text-white hover:bg-blue-500 cursor-pointer">
                 <p>{this.props.eventDetails.eventName}</p>
                 <p className="mt-2 text-4xl">{this.props.eventDetails.eventDate}</p>
                 <p className="mt-2">At {this.props.eventDetails.eventTime}</p>
--- resources/js/components/TrackingButton.js
+++ resources/js/components/TrackingButton.js
@@ -7,7 +7,7 @@ function TrackingButton(props) {
     var buttonContext = (tracking) ? buttonContext = "Stop" : buttonContext = "Start"
     return (
         <button
-            className={buttonColour + '-200 rounded p-3 w-1/5 mt-5 shadow-md hover:shadow-lg hover:border-transparent hover:text-white hover:' + buttonColour +'-500'}
+            className={buttonColour + '-200 rounded p-3 w-1/5 mt-5 shadow-md transition duration-500 hover:shadow-lg hover:border-transparent hover:text-white hover:' + buttonColour +'-500'}
             onClick={() => setTracking(!tracking)}>
             {buttonContext} Tracking Boots and Bars Time
         </button>
