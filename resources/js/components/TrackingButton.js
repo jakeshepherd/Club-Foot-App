0fdd78a39d5cec5368127258e6f64f5aea4b70d3
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-10 11:30:20 +0000

    Tidy up

--- resources/js/components/TrackingButton.js
+++ resources/js/components/TrackingButton.js
@@ -3,9 +3,10 @@ import React from 'react';
 class TrackingButton extends React.Component {
     render () {
         var buttonColour = (this.props.text === 'Stop') ? buttonColour = 'bg-red' : buttonColour = 'bg-green'
-        var style = buttonColour + '-200 rounded p-3 w-1/5 .shadow-md hover:shadow-lg hover:border-transparent hover:text-white hover:' + buttonColour +'-500'
         return (
-            <button className={style}>{this.props.text} Tracking Boots and Bars Time</button>
+            <button className={buttonColour + '-200 rounded p-3 w-1/5 .shadow-md hover:shadow-lg hover:border-transparent hover:text-white hover:' + buttonColour +'-500'}>
+                {this.props.text} Tracking Boots and Bars Time
+            </button>
         );
     }
 }
