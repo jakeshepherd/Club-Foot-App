f7afbaf4c442dcac3af3b7ece2bb62a389947f30
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-10 14:54:49 +0000

    Some more stlyinf

--- resources/js/components/DailyAdheranceView.js
+++ resources/js/components/DailyAdheranceView.js
@@ -8,13 +8,13 @@ function DailyAdheranceView(props) {
             {Object.entries(props.data).map(([key, value]) => {
                 if (value) {
                     return (
-                        <div key={key} className="daily-adherance border-green-400">
+                        <div key={key} className="daily-adherance border-green-400 transition duration-500 hover:bg-green-400">
                             {key[0]}
                         </div>
                     )
                 } else {
                     return (
-                        <div key={key} className="daily-adherance border-red-400">
+                        <div key={key} className="daily-adherance border-red-400 transition duration-500 hover:bg-red-400">
                             {key[0]}
                         </div>
                     )
