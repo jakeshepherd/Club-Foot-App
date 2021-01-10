1bfff7a1fa5e898542839f1cc241f42704abbe7b
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-10 14:29:21 +0000

    Add calendar event

--- resources/js/components/Homepage.js
+++ resources/js/components/Homepage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TrackingButton from './TrackingButton';
 import DailyAdheranceView from './DailyAdheranceView';
+import NextCalendarEvent from './NextCalendarEvent';
 
 import { PieChart } from 'react-minimal-pie-chart';
 
@@ -18,9 +19,9 @@ function Homepage() {
 
     return (
         <div className="p-5 m-auto w-10/12 text-center">
-            <h1>Your Adherance for the last 7 days</h1>
+            <h2 className="font-seoulNamsan text-xl font-bold">Your Adherance for the last 7 days</h2>
             <PieChart
-                className="m-auto w-1/4"
+                className="m-auto w-1/5"
                 data={[
                     { title: 'Time Completed', value: 85, color: '#a7f3d0' },
                     { title: 'Time left to go', value: 15, color: '#fda0a0' },
@@ -30,10 +31,18 @@ function Homepage() {
                 labelPosition={0}
                 label={() => {return '85%'}}
             />
-            <h2>14.5 hours a day on average</h2>
+            <h2 className="text-xl font-bold">14.5 hours a day on average</h2>
             <DailyAdheranceView data={adheranceData}/>
             <TrackingButton 
                 text="Start"
+            />
+            <h2 className="mt-5 text-xl font-bold">What's Coming up...</h2>
+            <NextCalendarEvent
+                eventDetails={{
+                    eventName: 'Meet With Physio',
+                    eventDate: '24th Dec',
+                    eventTime: '4pm',
+                }}
             />
         </div>
     );
