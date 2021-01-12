f2f5ac8bc48dd95646729037daaa927a583b8b99
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-12 20:03:59 +0000

    Add button to register on login bage

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,6 @@
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
@@ -52,5 +51,11 @@
                 </x-button>
             </div>
         </form>
+        <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md
+                        font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none
+                        focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-3"
+                type="button"
+                onclick="window.location='{{ route("register") }}'">
+            Register</button>
     </x-auth-card>
 </x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth'])->name('welcome');
 
 require __DIR__.'/auth.php';
