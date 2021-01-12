2e9377bad660337f0ab30aef07b19c664730023f
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-12 16:09:10 +0000

    Tidy up daily adherance

--- resources/js/components/DailyAdheranceView.js
+++ resources/js/components/DailyAdheranceView.js
@@ -6,19 +6,13 @@ function DailyAdheranceView(props) {
     return (
         <div>
             {Object.entries(props.data).map(([key, value]) => {
-                if (value) {
-                    return (
-                        <div key={key} className="daily-adherance border-green-400 transition duration-500 hover:bg-green-400 cursor-pointer">
-                            {key[0]}
-                        </div>
-                    )
-                } else {
-                    return (
-                        <div key={key} className="daily-adherance border-red-400 transition duration-500 hover:bg-red-400 cursor-pointer">
-                            {key[0]}
-                        </div>
-                    )
-                }
+                var circleColour = value ? 'green' : 'red'
+                var classStyle = 'daily-adherance border-' + circleColour + '-400 hover:bg-' + circleColour + '-400'
+                return (
+                    <div key={key} className={classStyle}>
+                        {key[0]}
+                    </div>
+                )
             })}
         </div>
     )
