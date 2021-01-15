88142b9bac138635a1dd4f2d4c70ad9c90d588a9
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-15 13:52:26 +0000

    Migration for boots and bars time

---
+++ database/migrations/2021_01_15_122733_create_boots_and_bars_times_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBootsAndBarsTimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boots_and_bars_times', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time')->nullable();
+            $table->dateTime('duration')->nullable();
+            $table->timestamps();
+            $table->index('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boots_and_bars_times');
+    }
+}
