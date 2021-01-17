3a52b30a774cae6f88dd1936b943759219b10018
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-17 12:36:17 +0000

    Add props for some reason

--- resources/js/components/TrackingButton.js
+++ resources/js/components/TrackingButton.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 class TrackingButton extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             tracking: false
         }
