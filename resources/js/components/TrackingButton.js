94aa7ab4428aa5fe43a00e710199ec4cbfc7ead6
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-17 12:34:06 +0000

    Start tracking on front end

--- resources/js/components/TrackingButton.js
+++ resources/js/components/TrackingButton.js
@@ -1,17 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function TrackingButton(props) {
-    const [tracking, setTracking] = useState(false);
+class TrackingButton extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            tracking: false
+        }
+    }
 
-    var buttonColour = (tracking) ? buttonColour = 'bg-red' : buttonColour = 'bg-green'
-    var buttonContext = (tracking) ? buttonContext = "Stop" : buttonContext = "Start"
-    return (
-        <button
-            className={buttonColour + '-200 rounded p-3 w-1/5 mt-5 shadow-md transition duration-500 hover:shadow-lg hover:border-transparent hover:text-white hover:' + buttonColour +'-500'}
-            onClick={() => setTracking(!tracking)}>
-            {buttonContext} Tracking Boots and Bars Time
-        </button>
-    );
+    startTracking() {
+        if (!this.state.tracking) {
+            axios.post('/start-tracking')
+                .then(r => {
+                    console.log(r.data)
+                    this.setState({tracking: !this.state.tracking})
+                })
+        } else {
+            this.setState({tracking: !this.state.tracking})
+        }
+    }
+
+    render() {
+        var buttonColour = (this.state.tracking) ? buttonColour = 'bg-red' : buttonColour = 'bg-green'
+        var buttonContext = (this.state.tracking) ? buttonContext = "Stop" : buttonContext = "Start"
+        return (
+            <button
+                className={buttonColour + '-200 rounded p-3 w-1/5 mt-5 shadow-md transition duration-500 hover:shadow-lg hover:border-transparent hover:text-white hover:' + buttonColour +'-500'}
+                onClick={() => {this.startTracking()}}>
+                {buttonContext} Tracking Boots and Bars Time
+            </button>
+        );
+    }
 }
 
 export default TrackingButton;
