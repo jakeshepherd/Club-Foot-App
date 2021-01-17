084340851315675accb6382f08736652a3450cd7
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-17 12:49:46 +0000

    Use json response w/ return typ

--- app/Http/Controllers/TimingController.php
+++ app/Http/Controllers/TimingController.php
@@ -5,17 +5,18 @@ namespace App\Http\Controllers;
 
 use App\Models\BootsAndBarsTime;
 use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
 class TimingController extends Controller
 {
-    public function startTracking()
+    public function startTracking(): JsonResponse
     {
         $bootsAndBarsRow = new BootsAndBarsTime();
         $bootsAndBarsRow->user_id = Auth::id();
         $bootsAndBarsRow->start_time = Carbon::now();
         $bootsAndBarsRow->save();
 
-        return response($bootsAndBarsRow->id, 201);
+        return response()->json($bootsAndBarsRow->id, 201);
     }
 }
