9fcdad4eb645aff3164cc3998a823ee6fd2f1755
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-24 12:29:42 +0000

    Add test for 8 days

--- tests/Feature/BootsTimingsHistoryTest.php
+++ tests/Feature/BootsTimingsHistoryTest.php
@@ -57,7 +57,44 @@ class BootsTimingsHistoryTest extends TestCase
         $this->assertSame($expected, $actual);
     }
 
-    // TODO gonna need some more tests to test how well the last 7 days thing works
+    public function test_it_doesnt_include_8_days_before() {
+        // setup
+        $minutesBootsWorn = [840,840,480,900,780,300,660,600];
+        $user = $this->createUserAndLogin();
+        $startTime = Carbon::now();
+        Carbon::setTestNow($startTime);
+
+        // Need to add a weeks worth of data
+        for ($i = 0; $i<8; $i++) {
+            $newRow = new BootsAndBarsTime;
+            $newRow->start_time = Carbon::now();
+            $newRow->end_time = Carbon::now()->addMinutes($minutesBootsWorn[$i]);
+            $newRow->duration = $minutesBootsWorn[$i];
+            $newRow->user_id = $user->id;
+            $newRow->tracking = false;
+            $newRow->save();
+
+            Carbon::now()->subHours($minutesBootsWorn[$i]);
+            $startTime->addDay();
+            Carbon::setTestNow($startTime);
+        }
+
+        // calculate average of the data in here
+        $expected = 0;
+        for($i = 1; $i<8; $i++) {
+            $expected += $minutesBootsWorn[$i];
+        }
+
+        $expected = (int) round($expected/7, 0);
+
+        // go to endpoint, get averaged data
+        $response = $this->get('/get-7-day-average');
+        $response->assertOk();
+        $actual = (int) $response->getContent();
+
+        // assert equal in minutes
+        $this->assertSame($expected, $actual);
+    }
 
     // TODO test it ignores anything less than like 10 mins cos not worth it
 }
