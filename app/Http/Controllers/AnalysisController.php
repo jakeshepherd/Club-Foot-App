bfef16c3952a66a48934e14c7aecc418f5a585ab
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-26 15:34:13 +0000

    Add some comments

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -12,18 +12,24 @@ class AnalysisController extends Controller
     {
         $durations = (new BootsAndBarsTime)->getSevenDayAverage();
         $averages = [];
+
+        // go through and get durations
         foreach ($durations as $duration) {
+            // if there are multiple timings on one day, we handle it slightly differently
             if (count($duration) > 1) {
                 $dailyTotal = 0;
                 foreach ($duration as $subTime) {
                     $dailyTotal += $subTime['duration'];
                 }
                 $averages[] = $dailyTotal;
-            } else {
+            }
+            // if there's just one timing for the day then we can just get the duration nicely
+            else {
                 $averages[] = $duration[0]['duration'];
             }
         }
 
+        // need to check here in case of /0 error.
         if (count($averages) > 0) {
             return response()->json(round(array_sum($averages)/count($averages), 0), 200);
         } else {
