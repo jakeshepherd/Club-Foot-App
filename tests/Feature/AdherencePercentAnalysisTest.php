7dd33f2cbf82f5b959e6030d3cda6228f02ed12b
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-27 12:34:37 +0000

    Confusing.

--- tests/Feature/AdherencePercentAnalysisTest.php
+++ tests/Feature/AdherencePercentAnalysisTest.php
@@ -76,9 +76,109 @@ class AdherencePercentAnalysisTest extends TestCase
         $newRow->duration = 18*60;
         $newRow->user_id = $user->id;
         $newRow->tracking = false;
-        $expected[$startTime->format('l')] = true;
         $newRow->save();
+        $expected[$startTime->format('l')] = true;
 
+        $response = $this->get('/weekly-adherence');
+        $response->assertOk();
+        $actual = json_decode($response->content(), true);
+
+        $this->assertSame($expected, $actual);
+    }
+    public function test_it_works_for_whole_week()
+    {
+        $expected = [];
+
+        $user = $this->createUserAndLogin();
+        $this->post('/set-boots-time-goal', [
+            'time_goal' => '15'
+        ]);
+
+        $startTime = Carbon::parse('2021-02-02 01:00:00');
+        Carbon::setTestNow($startTime);
+
+        // Need to add a weeks worth of data
+        $newRow = new BootsAndBarsTime;
+        $newRow->start_time =$startTime;
+        $newRow->end_time = $startTime->addMinutes(15*60);
+        $newRow->duration = 15*60;
+        $newRow->user_id = $user->id;
+        $newRow->tracking = false;
+        $newRow->save();
+        $startTime->subMinutes(15*60);
+        $expected[$startTime->format('l')] = true;
+
+        $startTime->addDay();
+
+        $newRow = new BootsAndBarsTime;
+        $newRow->start_time =$startTime;
+        $newRow->end_time = $startTime->addMinutes(12*60);
+        $newRow->duration = 12*60;
+        $newRow->user_id = $user->id;
+        $newRow->tracking = false;
+        $newRow->save();
+        $startTime->subMinutes(12*60);
+        $expected[$startTime->format('l')] = false;
+
+        $startTime->addDay();
+
+        $newRow = new BootsAndBarsTime;
+        $newRow->start_time =$startTime;
+        $newRow->end_time = $startTime->addMinutes(10*60);
+        $newRow->duration = 10*60;
+        $newRow->user_id = $user->id;
+        $newRow->tracking = false;
+        $newRow->save();
+        $startTime->subMinutes(10*60);
+        $expected[$startTime->format('l')] = false;
+
+        $startTime->addDay();
+
+        $newRow = new BootsAndBarsTime;
+        $newRow->start_time = $startTime;
+        $newRow->end_time = $startTime->addMinutes(16*60);
+        $newRow->duration = 16*60;
+        $newRow->user_id = $user->id;
+        $newRow->tracking = false;
+        $newRow->save();
+        $startTime->subMinutes(16*60);
+        $expected[$startTime->format('l')] = true;
+
+        $startTime->addDay();
+
+        $newRow = new BootsAndBarsTime;
+        $newRow->start_time =$startTime;
+        $newRow->end_time = $startTime->addMinutes(18*60);
+        $newRow->duration = 18*60;
+        $newRow->user_id = $user->id;
+        $newRow->tracking = false;
+        $newRow->save();
+        $startTime->subMinutes(18*60);
+        $expected[$startTime->format('l')] = true;
+        $startTime->addDay();
+
+        $newRow = new BootsAndBarsTime;
+        $newRow->start_time =$startTime;
+        $newRow->end_time = $startTime->addMinutes(18*60);
+        $newRow->duration = 18*60;
+        $newRow->user_id = $user->id;
+        $newRow->tracking = false;
+        $newRow->save();
+        $startTime->subMinutes(18*60);
+        $expected[$startTime->format('l')] = true;
+        $startTime->addDay();
+
+        $newRow = new BootsAndBarsTime;
+        $newRow->start_time =$startTime;
+        $newRow->end_time = $startTime->addMinutes(18*60);
+        $newRow->duration = 18*60;
+        $newRow->user_id = $user->id;
+        $newRow->tracking = false;
+        $newRow->save();
+        $startTime->subMinutes(18*60);
+        $expected[$startTime->format('l')] = true;
+
+        $startTime->addDays(1);
         $response = $this->get('/weekly-adherence');
         $response->assertOk();
         $actual = json_decode($response->content(), true);
