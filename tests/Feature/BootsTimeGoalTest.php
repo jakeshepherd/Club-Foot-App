dd8405dd4096a1c45594a8ae66616e0bcbf484aa
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-27 15:01:49 +0000

    remove unused imports

--- tests/Feature/BootsTimeGoalTest.php
+++ tests/Feature/BootsTimeGoalTest.php
@@ -4,8 +4,6 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
