7eb81a001da580691c1bde18a3282922afdc1796
Jake Shepherd <jakeshepherd98@gmail.com>
2021-01-31 13:43:37 +0000

    Add some images for FAQs

--- resources/js/components/FAQ/FAQSplash.js
+++ resources/js/components/FAQ/FAQSplash.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 
 import Question from "./Question";
 import skin from '../../../images/icons8-skin-80.png';
+import clothing from '../../../images/icons8-jumper-128.png';
+import boot from '../../../images/icons8-work-boot-64.png';
 
 class FAQSplash extends React.Component {
     render() {
         return (
             <div>
                 <Question name={"Skin Issues"} image={skin}/>
+                <Question name={"Clothing"} image={clothing}/>
+                <Question name={"Brace Advice"} image={boot}/>
             </div>
         );
     }
