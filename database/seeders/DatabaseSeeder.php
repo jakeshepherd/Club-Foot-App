503de0e9d67c4589da17daf8055c3deae452e28b
jakeshepherd <36624642+jakeshepherd@users.noreply.github.com>
2021-02-01 17:21:34 +0000

    🗃 Seed times table (#27)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\BootsAndBarsTime;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,10 +15,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory()->create([
-             'name' => 'jake',
-             'email' => 'jakeshepherd98@gmail.com',
-             'password' => '$2y$10$mJYZB0nTFwGfBT6Z61YQiO2YiP3YZpZaU6wpMUKBor1UK0KfwMLrm',
-         ]);
+        $user = \App\Models\User::factory()->create([
+            'name' => 'jake',
+            'email' => 'jakeshepherd98@gmail.com',
+            'password' => '$2y$10$mJYZB0nTFwGfBT6Z61YQiO2YiP3YZpZaU6wpMUKBor1UK0KfwMLrm',
+            'time_goal' => 14 * 60
+        ]);
+        BootsAndBarsTime::create([
+            'user_id' => $user->id,
+            'start_time' => Carbon::parse('-3 days -11 hours'),
+            'end_time' => Carbon::parse('-3 days +11 hours'),
+            'duration' => 11 * 60,
+            'tracking' => 0,
+        ]);
+        BootsAndBarsTime::create([
+            'user_id' => $user->id,
+            'start_time' => Carbon::parse('-2 days -16 hours'),
+            'end_time' => Carbon::parse('-2 days +16 hours'),
+            'duration' => 16 * 60,
+            'tracking' => 0,
+        ]);
+        BootsAndBarsTime::create([
+            'user_id' => $user->id,
+            'start_time' => Carbon::parse('-1 days -12 hours'),
+            'end_time' => Carbon::parse('-1 days +12 hours'),
+            'duration' => 12 * 60,
+            'tracking' => 0,
+        ]);
+        BootsAndBarsTime::create([
+            'user_id' => $user->id,
+            'start_time' => Carbon::parse('-15 hours'),
+            'end_time' => Carbon::now(),
+            'duration' => 15 * 60,
+            'tracking' => 0,
+        ]);
     }
 }
