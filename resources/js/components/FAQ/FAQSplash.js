72b68f254bf0a7e9d30451a7e1d801586ca2e5d8
Jake Shepherd <jakeshepherd98@gmail.com>
2021-02-01 17:08:28 +0000

    Dynamically load up FAQs

--- resources/js/components/FAQ/FAQSplash.js
+++ resources/js/components/FAQ/FAQSplash.js
@@ -10,9 +10,39 @@ class FAQSplash extends React.Component {
     render() {
         return (
             <div>
-                <Question name={"Skin Issues"} image={skin}/>
-                <Question name={"Clothing"} image={clothing}/>
-                <Question name={"Brace Advice"} image={boot}/>
+                <Question name={"Skin Issues"} image={skin}
+                          FAQs={[
+                              {
+                                  question: 'My child has blisters',
+                                  answer: 'Blistering is often a sign that the Boots and Bars are not fitting properly! ' +
+                                      'Please call your local practice and we will try and help.'
+                              },
+                              {
+                                  question: 'My child has a rash',
+                                  answer: 'A rash could mean that there is rubbing on your Boots and Bars, ' +
+                                      'please let us know by calling your local practice.'
+                              }
+                          ]}
+                />
+                <Question name={"Clothing"} image={clothing}
+                          FAQs={[
+                              {
+                                  question: 'Are there any good clothes to wear with the boots?',
+                                  answer: 'Try this website: www.example.com'
+                              },
+                          ]}/>
+                <Question name={"Brace Advice"} image={boot}
+                          FAQs={[
+                              {
+                                  question: 'How do I put on the boots and bars?',
+                                  answer: 'Take a look at this video!'
+                              },
+                              {
+                                  question: 'How long should my child wear the boots and bars for?',
+                                  answer: 'For the first 3 months your child should wear the boots for 23 hours a day. ' +
+                                      'After this, your child only needs to wear them between 12 and 14 hours a day'
+                              }
+                          ]}/>
             </div>
         );
     }
--- resources/js/components/FAQ/Question.js
+++ resources/js/components/FAQ/Question.js
@@ -10,40 +10,30 @@ import {
 // Demo styles, see 'Styles' section below for some notes on use.
 import 'react-accessible-accordion/dist/fancy-example.css';
 
-function Question(props) {
-    return (
-        <Accordion allowZeroExpanded className="p-2 w-1/2 m-2 ml-auto mr-auto rounded bg-white shadow-md">
-            <AccordionItem>
-                <AccordionItemHeading>
-                    <AccordionItemButton>
-                        <img className="w-9 inline-block" src={props.image} alt={'Skin icon'}/>
-                        <p className="ml-4 inline-block text-xl align-middle">{props.name}</p>
-                    </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel>
-                    <h1 className={"text-2xl"}>My child has blisters</h1>
-                    <p className="text-gray-500">
-                        Blistering is often a sign that the Boots and Bars are not fitting properly!
-                        Please call your local practice and we will try and help.
-                    </p>
-                </AccordionItemPanel>
-                <AccordionItemPanel>
-                    <h1 className={"text-2xl"}>My child has a rash</h1>
-                    <p className="text-gray-500">
-                        A rash could mean that there is rubbing on your Boots and Bars,
-                        please let us know by calling your local practice.
-                    </p>
-                </AccordionItemPanel>
-            </AccordionItem>
-        </Accordion>
+class Question extends React.Component {
+    render() {
+        return (
+            <Accordion allowZeroExpanded className="p-2 w-1/2 m-2 ml-auto mr-auto rounded bg-white shadow-md">
+                <AccordionItem>
+                    <AccordionItemHeading>
+                        <AccordionItemButton>
+                            <img className="w-9 inline-block" src={this.props.image} alt={'Skin icon'}/>
+                            <p className="ml-4 inline-block text-xl align-middle">{this.props.name}</p>
+                        </AccordionItemButton>
+                    </AccordionItemHeading>
+                    {this.props.FAQs.map((subQuestion, value) => (
+                        <AccordionItemPanel key={value}>
+                            <h1 className={"text-2xl"}>{subQuestion.question}</h1>
+                            <p className="text-gray-500">
+                                {subQuestion.answer}
+                            </p>
+                        </AccordionItemPanel>
+                    ))}
 
-
-        // <div className="p-2 w-1/2 m-2 ml-auto mr-auto rounded bg-white cursor-pointer shadow-md">
-        //     <img className="w-9 inline-block" src={props.image} alt={'Skin icon'}/>
-        //     <p className="ml-4 inline-block text-xl align-middle">{props.name}</p>
-        //     <img className="w-9 mr-6 float-right inline-block" src={dropdown} alt={'Dropdown icon'}/>
-        // </div>
-    );
+                </AccordionItem>
+            </Accordion>
+        );
+    }
 }
 
 export default Question;
