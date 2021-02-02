c43ed2844bbf9179da1bf77ed57b2740f437221b
Jake Shepherd <jakeshepherd98@gmail.com>
2021-02-02 17:54:24 +0000

    Backend for getting contact details

---
+++ app/Models/PhysioContactDetails.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PhysioContactDetails extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'email',
+        'phone_number',
+    ];
+}
---
+++ tests/Feature/PhysioDetailsTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\PhysioContactDetails;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PhysioDetailsTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_gets_physio_details()
+    {
+        $this->withoutExceptionHandling();
+        $user = $this->createUserAndLogin();
+        PhysioContactDetails::create([
+            'user_id' => $user->id,
+            'name' => 'Physio',
+            'email' => 'physio@gmail.com',
+            'phone_number' => '0123489',
+        ]);
+
+        $expected = [
+            'name' => 'Physio',
+            'email' => 'physio@gmail.com',
+            'phone_number' => '0123489',
+        ];
+
+        $response = $this->get('/contact-details');
+        $response->assertStatus(200);
+
+        $this->assertContains($expected, json_decode($response->getContent(), true));
+    }
+}
