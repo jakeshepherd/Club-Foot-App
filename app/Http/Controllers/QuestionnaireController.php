93615b6283ca9d905c26b815a5299ebb5422dee8
Jake Shepherd <jakeshepherd98@gmail.com>
2021-02-16 12:57:46 +0000

    Fix multiple on one day

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -15,11 +15,12 @@ class QuestionnaireController extends Controller
     public function setRoyeScoreQuestionnaire(): JsonResponse
     {
         // check if there is a questionnaire submitted today already and exit early
-        $thereIsARecordForCurrentDay = OutcomeQuestionnaireResult::where('id', Auth::id())
+        $thereIsARecordForCurrentDay = OutcomeQuestionnaireResult::where('user_id', Auth::id())
             ->whereDate('created_at', Carbon::today())
             ->get();
 
         if (!$thereIsARecordForCurrentDay->isEmpty()) {
+            Log::warning('Roye Score questionnaire already submitted today. Please try again tomorrow.');
             return response()->json(
                 'Roye Score questionnaire already submitted today. Please try again tomorrow.',
                 409
