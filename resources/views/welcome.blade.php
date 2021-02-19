d9b02fe470909c22878927576768edb48c3cb135
Jake Shepherd <jakeshepherd98@gmail.com>
2021-02-19 12:13:35 +0000

    Get rid of app.js and app.css from git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,11 +9,11 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="/css/app.css" rel="stylesheet">
     </head>
     <body>
         @include('layouts.navigation')
         <div id="homepage"></div>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="/js/app.js"></script>
     </body>
 </html>
