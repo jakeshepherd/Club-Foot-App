d6d0fb6bf9ee6081ce09ede9f2b175b4e2866d61
Jake Shepherd <jakeshepherd98@gmail.com>
2021-02-23 21:38:06 +0000

    Update template

--- resources/views/email/user-inactivity.blade.php
+++ resources/views/email/user-inactivity.blade.php
@@ -4,7 +4,7 @@
     We noticed you haven't logged in for a while and we just wanted to send you a little reminder.
 
     Compliance to your Boots and Bars directly affects the outcome of the treatment,
-    using your Club Foot dashboard will help you keep track of how the treatment is progression.
+    using your Club Foot dashboard will help you keep track of how the treatment is progressing.
 
 @component('mail::button', ['url' => 'https://salty-gorge-09035.herokuapp.com/welcome'])
     View your Dashboard
