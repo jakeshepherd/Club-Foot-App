bb24c166b8dad2088a64b5d77b7185f01a576c5a
Jake Shepherd <jakeshepherd98@gmail.com>
2021-03-17 15:00:50 +0000

    Update FAQ

--- resources/js/components/FAQ/FAQSplash.js
+++ resources/js/components/FAQ/FAQSplash.js
@@ -33,7 +33,7 @@ class FAQSplash extends React.Component {
                                                'the next size boots fitted.'
                                        },
                                        {
-                                           question: 'Boots Padding',
+                                           question: 'Bar Padding',
                                            answer: 'If you pad the bar you can help protect your child. ' +
                                                'You can use any kind of fabric that will act as a bumper.'
                                        }
