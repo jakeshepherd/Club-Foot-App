44e2a65ab87cdff6c85130ceac838f075da68b55
jakeshepherd <36624642+jakeshepherd@users.noreply.github.com>
2021-03-18 09:46:58 +0000

    🩹 Fix lots of issues (#80)
    * Initial clear button
    
    * Make it 4x1
    
    * Had to do this in the worst way
    
    * Mobile friendly and new answer
    
    * Use hours and mins

--- resources/js/components/OutcomeQuestionnaire/OutcomeQuestionTwoAnswers.js
+++ resources/js/components/OutcomeQuestionnaire/OutcomeQuestionTwoAnswers.js
@@ -6,8 +6,6 @@ class OutcomeQuestion extends React.Component {
         this.state = {
             1: false,
             2: false,
-            3: false,
-            4: false,
         }
     }
 
@@ -16,8 +14,6 @@ class OutcomeQuestion extends React.Component {
         this.setState({
             1: false,
             2: false,
-            3: false,
-            4: false,
             [answerId]: !this.state.[answerId],
         })
         this.props.handleQuestionSubmit(this.props.questionNumber, answerId)
@@ -26,21 +22,15 @@ class OutcomeQuestion extends React.Component {
     render() {
         const oneColour = this.state.[1] ? 'green-500' : 'white';
         const twoColour = this.state.[2] ? 'green-500' : 'white';
-        const threeColour = this.state.[3] ? 'green-500' : 'white';
-        const fourColour = this.state.[4] ? 'green-500' : 'white';
 
         return (
             <div className={"w-11/12 lg:w-1/2 m-2 mt-14 m-auto rounded p-4 shadow-md text-center bg-blue-100"}>
                 <h1>{this.props.question}</h1>
-                <div className={"grid md:grid-cols-4 grid-cols-1 mt-4"}>
+                <div className={"grid md:grid-cols-2 grid-cols-1 mt-4"}>
                     <p className={"shadow-md rounded p-2 m-2 w-24 justify-self-center cursor-pointer transition duration-500 bg-" + oneColour}
                        onClick={() => this.selectAnswer(1)}>{this.props.answers.one}</p>
                     <p className={"shadow-md rounded p-2 m-2 w-24 justify-self-center cursor-pointer transition duration-500 bg-" + twoColour}
                        onClick={() => this.selectAnswer(2)}>{this.props.answers.two}</p>
-                    <p className={"shadow-md rounded p-2 m-2 w-24 justify-self-center cursor-pointer transition duration-500 bg-" + threeColour}
-                       onClick={() => this.selectAnswer(3)}>{this.props.answers.three}</p>
-                    <p className={"shadow-md rounded p-2 m-2 w-24 justify-self-center cursor-pointer transition duration-500 bg-" + fourColour}
-                       onClick={() => this.selectAnswer(4)}>{this.props.answers.four}</p>
                 </div>
             </div>
         )
